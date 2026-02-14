176fb46fb39b3bc94d2f838bb2657e3bb72f8c48
uncle-brother-son <wayne@unclebrotherson.com>
2026-02-14 23:39:26 +0000

    Fix Sanity image type import for production build

--- web/lib/sanity/image.ts
+++ web/lib/sanity/image.ts
@@ -1,6 +1,6 @@
 import imageUrlBuilder from '@sanity/image-url'
 import { client } from './client'
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import type { SanityImageSource } from '@sanity/image-url'
 
 const builder = imageUrlBuilder(client)
 
